e7c2141d5abf54b59b5601384207bee8a34603a0
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-12-08 17:22:59 +0200

    feat: dockerize the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImportFeedsModule } from './tasks/feeds/import-feeds.module';
+import env from './common/utils/env.helper';
 import validateEnvironmentVariables from './common/validators/config.validator';
 
 @Module({
@@ -14,6 +16,18 @@ import validateEnvironmentVariables from './common/validators/config.validator';
     }),
     ScheduleModule.forRoot(),
     ImportFeedsModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: env('EUROGAMER_DATABASE_HOST'),
+        port: env.int('EUROGAMER_DATABASE_PORT'),
+        username: env('EUROGAMER_DATABASE_USER'),
+        password: env('EUROGAMER_DATABASE_PASSWORD'),
+        database: env('EUROGAMER_DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: env.bool('EUROGAMER_DATABASE_SYNCHRONIZE'),
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
