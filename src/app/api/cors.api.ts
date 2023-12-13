a7f870d4a00efd6ff65b7582a5c5c100700a35f3
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2023-12-13 11:26:13 +0200

    chore: remove cors mode

--- src/app/api/cors.api.ts
+++ src/app/api/cors.api.ts
@@ -1,15 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 
 function addCors(app: INestApplication) {
-  const origin = ['http://localhost:*'];
-
-  const methods = ['GET'];
-
-  app.enableCors({
-    origin,
-    methods,
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+  app.enableCors();
 }
 
 export default addCors;
